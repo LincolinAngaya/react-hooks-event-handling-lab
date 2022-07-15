68529d0b0e71bf93763ac10df798c9bbe0e3477c
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-15 10:52:14 +0300

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from 'react'
+
+const Keypad = () => {
+  function fireEvent(){
+console.log("Entering password...")
+    }
+  return (
+    
+     <input onChange={fireEvent} type="password"/>
+  )
+}
+
+export default Keypad;
